b3968c4db128eeb686f4d922deb61475b1f167d7
Romina Pineda <romi.macka@gmail.com>
2020-10-04 14:31:34 -0300

    test

--- carta.rb
+++ carta.rb
@@ -1,7 +1,9 @@
-class Letter
+class Carta
     attr_accessor :numero, :pinta
     def initialize (numero, pinta)
         @numero = numero
-        @pinta = pinta
+        @pinta = pinta.sample
     end
-    
+    numero = Random.rand(1,13)
+    pinta = ['C','D','E','T']
+end
